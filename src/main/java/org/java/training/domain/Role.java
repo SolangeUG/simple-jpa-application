10838a9d08c22760e82c2da4bfc330966a53ef25
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-13 21:20:39 +0200

    Add domain objects

---
+++ src/main/java/org/java/training/domain/Role.java
@@ -0,0 +1,108 @@
+package org.java.training.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Bean class that represents the role given to a user
+ * @author Solange U. Gasengayire
+ */
+@Entity
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long roleId;
+
+    @Column
+    private String roleName;
+
+    @ManyToMany
+    private List<User> users;
+
+    /**
+     * Default constructor
+     */
+    public Role() {
+        super();
+    }
+
+    public Role(String name) {
+        this.roleName = name;
+    }
+
+    /**
+     * Return this entity's ID
+     * @return roleId
+     */
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    /**
+     * Set this role's id
+     * @param id the id to be set
+     */
+    public void setRoleId(Long id) {
+        this.roleId = id;
+    }
+
+    /**
+     * Return this role given name
+     * @return roleName
+     */
+    public String getRoleName() {
+        return roleName;
+    }
+
+    /**
+     * Set this role's name
+     * @param name the name to be set
+     */
+    public void setRoleName(String name) {
+        this.roleName = name;
+    }
+
+    /**
+     * Return a list of users with this role
+     * @return users
+     */
+    public List<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * Set this role's user list
+     * @param userList the user list to be set
+     */
+    public void setUsers(List<User> userList) {
+        this.users = userList;
+    }
+
+    /**
+     * Returns a string representation of the object. In general, the
+     * {@code toString} method returns a string that
+     * "textually represents" this object. The result should
+     * be a concise but informative representation that is easy for a
+     * person to read.
+     * It is recommended that all subclasses override this method.
+     * <p>
+     * The {@code toString} method for class {@code Object}
+     * returns a string consisting of the name of the class of which the
+     * object is an instance, the at-sign character `{@code @}', and
+     * the unsigned hexadecimal representation of the hash code of the
+     * object. In other words, this method returns a string equal to the
+     * value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return a string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return String.format("User [roleId=%s, roleName=%s]", roleId, roleName);
+    }
+}
--- src/main/java/org/java/training/domain/User.java
+++ src/main/java/org/java/training/domain/User.java
@@ -1,23 +1,25 @@
-package org.java.training.user;
+package org.java.training.domain;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Bean class that represents a user.
  * @author Solange U. Gasengayire
  */
 @Entity
-@NamedQuery(
-        query="SELECT u FROM User u",
-        name = "query_find_all_users"
-)
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private String role;
+    private Long userId;
+
+    @Column
+    private String userName;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Role> roles;
 
     /**
      * No-argument constructor
@@ -29,37 +31,59 @@ public class User {
 
     /**
      * Constructor
-     * @param name the user's name
-     * @param role the user's role
+     * @param userName the user's name
      */
-    public User(String name, String role) {
+    public User(String userName) {
         super();
-        this.name = name;
-        this.role = role;
+        this.userName = userName;
     }
 
     /**
      * Return this entity's ID
-     * @return the id
+     * @return the userId
      */
-    public Long getId() {
-        return id;
+    public Long getUserId() {
+        return userId;
+    }
+
+    /**
+     * Set this user's id
+     * @param id the id to be set
+     */
+    public void setUserId(Long id) {
+        this.userId = id;
     }
 
     /**
      * Return this user's name
-     * @return the name
+     * @return the userName
      */
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
     /**
-     * Return this user's role
-     * @return the role
+     * Set this user's name
+     * @param name the name to be set
      */
-    public String getRole() {
-        return role;
+    public void setUserName(String name) {
+        this.userName = name;
+    }
+
+    /**
+     * Return this user's roles
+     * @return the roles
+     */
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    /**
+     * Set this user's roles
+     * @param roleList the roles to be set
+     */
+    public void setRoles(List<Role> roleList) {
+        this.roles = roleList;
     }
 
     /**
@@ -68,6 +92,6 @@ public class User {
      */
     @Override
     public String toString() {
-        return String.format("User [id=%s, name=%s, role=%s]", id, name, role);
+        return String.format("User [userId=%s, userName=%s]", userId, userName);
     }
 }
