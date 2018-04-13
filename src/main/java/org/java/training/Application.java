edca3876ee43be4523985ce6a50fe9fd9369244e
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-13 21:26:22 +0200

    Refactor spring boot application entry point

--- src/main/java/org/java/training/Application.java
+++ src/main/java/org/java/training/Application.java
@@ -1,4 +1,4 @@
-package org.java.training.application;
+package org.java.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
