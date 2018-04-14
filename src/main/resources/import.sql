7c98f83a4676c6d5c7c7879f32710ae5d4e71733
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-14 16:36:36 +0200

    Add initial data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,24 @@
+-- Some Initial Data
+INSERT 
+    INTO Role (roleId, roleName)
+    VALUES (101, 'Administrator');
+
+INSERT
+    INTO Role (roleId, roleName)
+    VALUES (102, 'Site Administrator');
+
+INSERT
+    INTO Role (roleId, roleName)
+    VALUES (103, 'Senior Manager');
+
+INSERT
+    INTO Role (roleId, roleName)
+    VALUES (104, 'Site Manager');
+
+INSERT
+    INTO Role (roleId, roleName)
+    VALUES (105, 'Field Worker');
+
+INSERT
+    INTO Role (roleId, roleName)
+    VALUES (106, 'User');
