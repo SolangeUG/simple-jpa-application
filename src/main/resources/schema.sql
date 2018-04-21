28afea6ae728540277dfc0f1958f3c82ab2bec28
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-21 12:06:53 +0200

    Add Users table DDL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,15 @@
 -- roles : store roles values
 CREATE TABLE IF NOT EXISTS roles (
-    roleId          INT         PRIMARY KEY,
-    roleName        VARCHAR     NOT NULL,
+    roleId      INT             NOT NULL,
+    roleName    VARCHAR(255)    NOT NULL,
+    PRIMARY KEY(roleId)
+);
+
+-- users : store users values
+CREATE TABLE IF NOT EXISTS users (
+    userId      INT             NOT NULL,
+    userName    VARCHAR(255)    NOT NULL,
+    firstName   VARCHAR(255)    NOT NULL,
+    lastName    VARCHAR(255)    NOT NULL,
+    PRIMARY KEY(userId)
 );
