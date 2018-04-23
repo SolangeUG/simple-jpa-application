eab28c93186ef7bd78d3ad79d796dd43511cbbab
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-23 10:35:21 +0200

    Fix initial data insertion statements

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,18 +1,12 @@
 -- Some Initial Data
-INSERT INTO Role (roleId, roleName)
-    VALUES (101, 'Administrator');
+INSERT INTO Role VALUES (101, 'Administrator');
 
-INSERT INTO Role (roleId, roleName)
-    VALUES (102, 'Site Administrator');
+INSERT INTO Role VALUES (102, 'Site Administrator');
 
-INSERT INTO Role (roleId, roleName)
-    VALUES (103, 'Senior Manager');
+INSERT INTO Role VALUES (103, 'Senior Manager');
 
-INSERT INTO Role (roleId, roleName)
-    VALUES (104, 'Site Manager');
+INSERT INTO Role VALUES (104, 'Site Manager');
 
-INSERT INTO Role (roleId, roleName)
-    VALUES (105, 'Field Worker');
+INSERT INTO Role VALUES (105, 'Field Worker');
 
-INSERT INTO Role (roleId, roleName)
-    VALUES (106, 'User');
+INSERT INTO Role VALUES (106, 'User');
