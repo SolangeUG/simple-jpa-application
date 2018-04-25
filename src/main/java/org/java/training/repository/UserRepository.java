b556f1cc8687a94b4c2e872aae3c32b5aa1c8177
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-25 21:26:17 +0200

    Add methods to find users by their lastname and firstname

--- src/main/java/org/java/training/repository/UserRepository.java
+++ src/main/java/org/java/training/repository/UserRepository.java
@@ -4,6 +4,8 @@ import org.java.training.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * This is the JPA specific repository interface to manage our User entity
  * @author Solange U. Gasengayire
@@ -16,28 +18,20 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * @param userName the username criterion
      * @return the corresponding user
      */
-    default User findByUserName(String userName) {
-        // TODO : implement
-        return  null;
-    }
+    User findByUserName(String userName);
 
     /**
      * Find a user by their first name
      * @param firstName the firstname criterion
      * @return the corresponding user
      */
-    default User findByFirstName(String firstName) {
-        // TODO: implement
-        return null;
-    }
+    List<User> findByFirstName(String firstName);
 
     /**
      * Find a user by their last name
      * @param lastName the lastname criterion
      * @return the corresponding user
      */
-    default User findByLastName(String lastName) {
-        // TODO: implement
-        return null;
-    }
+    List<User> findByLastName(String lastName);
+
 }
